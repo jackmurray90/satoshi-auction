76365439ce4ad335f925626cb1252f33610ff7ab
Jack Murray <jack@murray.software>
2022-12-27 19:47:09 +0000

    remove bitcoin address validation that was broken

--- bitcoin.py
+++ bitcoin.py
@@ -21,10 +21,6 @@ def decode_base58(bc, length):
   return n.to_bytes(length, 'big')
 
 def valid_bitcoin_address(bc):
-  if len(bc) < 25 or len(bc) > 34:
-    return False
-  if bc[0] != '1' and bc[0] != '3':
-    return False
   try:
     bcbytes = decode_base58(bc, 25)
     return bcbytes[-4:] == sha256(sha256(bcbytes[:-4]).digest()).digest()[:4]
